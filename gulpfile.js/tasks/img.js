65c4827e472a34c10fe323bb3e2faee9ef8afcdb
Peter Antonius <peter.antonius@whitespace.se>
2016-11-30 17:25:50 +0100

    Added images sync and optimized

---
+++ gulpfile.js/tasks/img.js
@@ -0,0 +1,22 @@
+const config      = require('../../config')
+if(!config.tasks.images) return
+
+const changed     = require('gulp-changed')
+const gulp        = require('gulp')
+const imagemin    = require('gulp-imagemin')
+const path        = require('path')
+
+const paths = {
+  src: path.join(config.root.src, config.tasks.images.src, '/**/*.{' + config.tasks.images.extensions + '}'),
+  dest: path.join(config.root.dest, config.tasks.images.dest)
+}
+
+const imagesTask = function() {
+  return gulp.src([paths.src])
+    .pipe(changed(paths.dest)) // Ignore unchanged files
+    .pipe(imagemin()) // Optimize
+    .pipe(gulp.dest(paths.dest))
+}
+
+gulp.task('images', imagesTask)
+module.exports = imagesTask

a04f106cc3479d753be77d7d33eb2e746aad1612
Johan De Geer <johan.de.geer@whitespace.se>
2016-11-30 17:43:08 +0400

    Added gulp-svg-icon & example template

---
+++ gulpfile.js/tasks/icons.js
@@ -0,0 +1,19 @@
+const config = require('../../config')
+
+const gulp      = require('gulp')
+const svgSprite = require('gulp-svg-sprite')
+const path      = require('path')
+
+const paths = {
+  src: path.join(config.root.src, config.tasks.icons.src, '/**/*.svg'),
+  dest: path.join(config.tasks.icons.dest)
+}
+
+const iconsTask = function (cb) {
+  return gulp.src(paths.src)
+  .pipe(svgSprite(config.tasks.icons.config))
+  .pipe(gulp.dest(paths.dest));
+}
+
+gulp.task('icons', iconsTask)
+module.exports = iconsTask

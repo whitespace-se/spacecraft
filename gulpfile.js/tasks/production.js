4077ff65a2f5f81469e7d45bed1333cff7901397
Johan De Geer <johan.de.geer@whitespace.se>
2017-03-10 12:03:38 +0100

    Refactoring. Added function that removes "production" folder

--- gulpfile.js/tasks/production.js
+++ gulpfile.js/tasks/production.js
@@ -2,31 +2,39 @@ const config       = require('../../config')
 const gulp         = require('gulp')
 const gulpSequence = require('gulp-sequence')
 const getEnabledTasks = require('../lib/getEnabledTasks')
-const path = require('path')
+const del          = require('del')
+const path         = require('path')
 
 const productionTask = function(cb) {
   global.production = true
 
-  if(typeof config.production.tasks != 'undefined'){
-  	gulpSequence(config.production.tasks, function(){
-	  	var buildPath = config.production.path.src ? config.production.path.src : config.root.path;
-	  	var destPath = config.production.path.dest ? config.production.path.dest : config.root.production;
-		
-		if(config.production.folders){
-			var folderName;
-			for(var i = 0; i < config.production.folders.length; i++){
-				folderName = config.production.folders[i];
-				gulp.src(path.join(buildPath, folderName,"/*"))
-					.pipe(gulp.dest(path.join(destPath, folderName)));
-				console.log("Moving " + path.join(buildPath, folderName) + " to " + path.join(destPath, folderName));
-			}
-		}
-	});
+  if(config.production.tasks){
+    const buildPath = config.root.dest;
+    const destPath = config.production.path.dest ? config.production.path.dest : config.root.production;
+
+    gulpSequence(function(){
+      del([destPath], {
+        force: true
+      });
+    }, config.production.tasks, function(){
+      if(config.production.folders){
+        config.production.folders.forEach(function(folderName) {
+          gulp.src(path.join(buildPath, folderName,'/**'))
+            .pipe(gulp.dest(path.join(destPath, folderName)));
+          console.log('Moving ' + path.join(buildPath, folderName) + ' to ' + path.join(destPath, folderName));
+        })
+      }
+      else{
+          gulp.src(path.join(buildPath,'/**'))
+            .pipe(gulp.dest(destPath));
+      }
+
+    });
   }
   else{
-	  const tasks = getEnabledTasks('production')
-	  gulpSequence('clean', tasks.assetTasks, tasks.codeTasks, 'fractalBuild', cb)
-	}
+    const tasks = getEnabledTasks('production')
+    gulpSequence('clean', tasks.assetTasks, tasks.codeTasks, 'fractalBuild', cb)
+  }
 }
 
 gulp.task('production', productionTask)

430094e6872cd0f95201d93e110a818863762d8a
Johan Veeborn <johan.veeborn@whitespace.se>
2017-03-10 10:17:36 +0100

    Updated productiontask to take taskchain and folders to copy from config

--- gulpfile.js/tasks/production.js
+++ gulpfile.js/tasks/production.js
@@ -2,11 +2,31 @@ const config       = require('../../config')
 const gulp         = require('gulp')
 const gulpSequence = require('gulp-sequence')
 const getEnabledTasks = require('../lib/getEnabledTasks')
+const path = require('path')
 
 const productionTask = function(cb) {
   global.production = true
-  const tasks = getEnabledTasks('production')
-  gulpSequence('clean', tasks.assetTasks, tasks.codeTasks, 'fractalBuild', cb)
+
+  if(typeof config.production.tasks != 'undefined'){
+  	gulpSequence(config.production.tasks, function(){
+	  	var buildPath = config.production.path.src ? config.production.path.src : config.root.path;
+	  	var destPath = config.production.path.dest ? config.production.path.dest : config.root.production;
+		
+		if(config.production.folders){
+			var folderName;
+			for(var i = 0; i < config.production.folders.length; i++){
+				folderName = config.production.folders[i];
+				gulp.src(path.join(buildPath, folderName,"/*"))
+					.pipe(gulp.dest(path.join(destPath, folderName)));
+				console.log("Moving " + path.join(buildPath, folderName) + " to " + path.join(destPath, folderName));
+			}
+		}
+	});
+  }
+  else{
+	  const tasks = getEnabledTasks('production')
+	  gulpSequence('clean', tasks.assetTasks, tasks.codeTasks, 'fractalBuild', cb)
+	}
 }
 
 gulp.task('production', productionTask)

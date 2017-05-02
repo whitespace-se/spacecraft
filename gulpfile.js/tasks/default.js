03b70ab94a2d8f5c73f07dbe3b99f4cc63b94cc9
Johan De Geer <johan.de.geer@whitespace.se>
2017-05-02 20:53:36 +0400

    Added proxy task

--- gulpfile.js/tasks/default.js
+++ gulpfile.js/tasks/default.js
@@ -1,10 +1,21 @@
+const config          = require('../../config')
 const gulp            = require('gulp')
 const gulpSequence    = require('gulp-sequence')
 const getEnabledTasks = require('../lib/getEnabledTasks')
 
 const defaultTask = function(cb) {
   const tasks = getEnabledTasks('watch')
-  gulpSequence('clean', tasks.assetTasks, tasks.codeTasks, 'watch', 'fractalStart', cb)
+
+  // If type not set - run fractal
+  let type;
+  if(!config.type){
+    type = 'fractalStart'
+  }
+  else{
+    type = config.type
+  }
+
+  gulpSequence('clean', tasks.assetTasks, tasks.codeTasks, 'watch', type, cb)
 }
 
 gulp.task('default', defaultTask)
---
+++ gulpfile.js/tasks/proxy.js
@@ -0,0 +1,23 @@
+const config          = require('../../config')
+const gulp            = require('gulp')
+const browserSync     = require('browser-sync')
+const watch           = require('gulp-watch')
+const path            = require('path')
+
+const proxyTask = function(cb) {
+  let bs = browserSync.init(config.proxy.browserSyncConfig)
+
+  let css = path.join(config.root.dest, config.tasks.css.dest, '/**/*.css')
+  gulp.watch(css, function() {
+    gulp.src(css)
+      .pipe(bs.stream())
+  })
+
+  let js = path.join(config.root.dest, config.tasks.js.dest, '/**/*.js')
+  gulp.watch(js, function() {
+    bs.reload()
+  })
+}
+
+gulp.task('proxy', proxyTask)
+module.exports = proxyTask

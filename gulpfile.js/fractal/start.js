5f1e2b093e314986b19d9bdbf7ceb570064364a6
Johan De Geer <johan.de.geer@whitespace.se>
2017-07-19 08:25:45 +0400

    Fixed proxy settings

--- gulpfile.js/fractal/start.js
+++ gulpfile.js/fractal/start.js
@@ -5,7 +5,7 @@ const logger          = fractal.cli.console;
 
 const fractalStart = function () {
   // Don't run fractal if config.proxy is set
-  if(config.proxy.browserSyncConfig) return
+  if(config.proxy) return
 
   const server = fractal.web.server({
       sync: true
